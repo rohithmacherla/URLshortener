36f0ea407a2d91acea4a54be7c69e57d2f307584
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-28 19:06:00 -0400

    Added a route for API which outputs JSON in the correct format

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/:id(*)', function(req, res) {
+    var id = req.params.id;
+    var obj = {
+        orginalURL: id,
+        short: "www.website.com/8493"
+    };
+    res.send(obj);
 });
 
 module.exports = router;
