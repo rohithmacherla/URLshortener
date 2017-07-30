8f478805bd1bc9d53dd9f3d32fbf341dafd27c71
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-29 21:29:06 -0400

    Set up the mongoDB server and connected client to local server

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var mongo = require('mongodb').MongoClient;
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
@@ -12,6 +12,22 @@ router.get('/:id(*)', function(req, res) {
         orginalURL: id,
         short: "www.website.com/8493"
     };
+    var url = "mongodb://localhost:27017/test";
+    if (req.url === '/favicon.ico') {
+        r.end();
+        console.log('favicon requested');
+        return;
+    }
+
+
+
+    mongo.connect(url, function(err, db) {
+        if(err) throw err;
+        db.createCollection('nodes');
+        var nodes = db.collection('nodes');
+        nodes.insert({"test":"This is a test"});
+        db.close();
+    });
     res.send(obj);
 });
 
