a0caf61e2539f6af86e98ffa8fca32872edef31d
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-30 19:14:14 -0400

    Added mongoose and imported it

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,50 @@
+/*jshint esversion:6*/
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-
-/*
-https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
-I didn't write this method, but this is less bug prone than my initial plan so I'll 
-be using this :). 
-*/
-function validURL(str) {
-  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-    '(\#[-a-z\d_]*)?$','i'); // fragment locater
-  if(!pattern.test(str)) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
+var urlcheck = require('valid-url');
 
 
 router.get('/:id(*)', function(req, res) {
     if (req.url === '/favicon.ico') {
-        r.end();
+        res.end();
         return;
     }
+    var url = "mongodb://localhost:27017/test";
     var id = req.params.id;
-    if(validURL(id)) {
-        var obj = {
+    var obj = {
             orginalURL: id,
             short: "www.website.com/8493"
-        };
-        var url = "mongodb://localhost:27017/test";
+    };
+
+    //if(id.indexOf('.com') > -1 || id.indexOf('.net') > -1 
+     //   || id.indexOf('.org') > -1 || id.indexOf('.gov') > -1) {
+        //if id is just numbers then look into the database for the original URL and redirect
+        //if it's not my website, then add it to the databse and print JSON
+    
         mongo.connect(url, function(err, db) {
             if(err) throw err;
-            db.createCollection('nodes');
-            var nodes = db.collection('nodes');
-            nodes.insert({"test":"This is a test"});
+            
+            if(!isNaN(id)) {
+                //connect to database to find original and redirect
+                var og = db.collection('nodes').find({"short": id});
+                if(og !== null) {
+                    res.redirect(og.orginalURL);
+                } else {
+                    res.send("Not Valid");
+                }
+            } else {
+                //put this into the database and return json
+                var collection = db.collection('nodes');
+                var hash = Math.floor(Math.random()*100000);
+                collection.insert({"orginalURL": id, "short": hash});
+                res.send({"orginalURL": id, "short": hash});
+            }
             db.close();
         });
-        res.send(obj);
-    }
-    
+    //} else {
+    //    res.send({orginalURL:id, "short": null});
+    //}
 });
 
 module.exports = router;
