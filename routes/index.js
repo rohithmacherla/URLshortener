e850c1a339b8dfa24632896e34c28f8ee7230b03
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-29 22:05:53 -0400

    Added a method to check for valid URLS

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,50 @@ var mongo = require('mongodb').MongoClient;
 //   res.render('index', { title: 'Express' });
 // });
 
+
+/*
+https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+I didn't write this method, but this is less bug prone than my initial plan so I'll 
+be using this :). 
+*/
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+  if(!pattern.test(str)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
+
 router.get('/:id(*)', function(req, res) {
-    var id = req.params.id;
-    var obj = {
-        orginalURL: id,
-        short: "www.website.com/8493"
-    };
-    var url = "mongodb://localhost:27017/test";
     if (req.url === '/favicon.ico') {
         r.end();
-        console.log('favicon requested');
         return;
     }
-
-
-
-    mongo.connect(url, function(err, db) {
-        if(err) throw err;
-        db.createCollection('nodes');
-        var nodes = db.collection('nodes');
-        nodes.insert({"test":"This is a test"});
-        db.close();
-    });
-    res.send(obj);
+    var id = req.params.id;
+    if(validURL(id)) {
+        var obj = {
+            orginalURL: id,
+            short: "www.website.com/8493"
+        };
+        var url = "mongodb://localhost:27017/test";
+        mongo.connect(url, function(err, db) {
+            if(err) throw err;
+            db.createCollection('nodes');
+            var nodes = db.collection('nodes');
+            nodes.insert({"test":"This is a test"});
+            db.close();
+        });
+        res.send(obj);
+    }
+    
 });
 
 module.exports = router;
